56a59489405e92cd112e8556e121b09222edc81b
filipmellgren <filip.mellgren@gmail.com>
2020-08-22 18:28:24 +0200

    Functioning shiny output

--- asset-projection/app.R
+++ asset-projection/app.R
@@ -10,7 +10,12 @@
 library(shiny)
 library(tidyverse)
 library(rio)
-df <- import("../data/asset_development.csv")
+library(patchwork)
+source("../code/aux_functions.R")
+source("../code/savings.R")
+#df <- import("../data/asset_development.csv")
+colorpal <- scale_color_brewer(type = 'qual', palette = 6)
+
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -21,20 +26,87 @@ ui <- fluidPage(
             selectInput("line_plot", 
                 label = "Choose a color",
                 choices = c("coral", "turquoise"),
-                selected = "coral")
+                selected = "coral"),
+            
+            sliderInput("start_wage", h3("Net wage (per month)"),
+                        min = 0, max = 100000, value = 20000),
+            
+            sliderInput("wage_growth", h3("Annual wage growth"),
+                        min = 0, max = 0.1, value = 0.03),
+            
+            sliderInput("start_wealth", h3("Today's wealth"),
+                        min = 0, max = 10^7, value = 7*10^5),
+            
+            sliderInput("consumption", h3("Consumption & housing"),
+                        min = 0, max = 10^4, value = 7*10^3),
+            
+            sliderInput("cons_growth", h3("Annual consumption growth"),
+                        min = 0, max = 0.1, value = 0.05)
             ),
         
         mainPanel(
-            plotOutput("line_plot")
+            plotOutput("line_plot"),
+            plotOutput("start_wage"),
+            plotOutput("start_wealth"),
+            plotOutput("consumption"),
+            plotOutput("cons_growth")
         )
     )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+    time_hz <- 35
+    df <- tibble(.rows = time_hz)
     output$line_plot <- renderPlot({ 
-        df %>% ggplot(aes(x = year, y = assets)) +
-            geom_line(color = input$line_plot)
+        # Year
+        df <- df %>% mutate(year = 1:time_hz)
+        
+        # Wage
+        df <- df %>% mutate(wage = growth.seq(input$wage_growth, time_hz, input$start_wage))
+        
+        # Consumption & housing
+        df <- df %>% mutate(consumption = growth.seq(input$cons_growth, time_hz, input$consumption))
+        
+        # CSN
+        csn_cf <- import("../data/csn.csv") %>% 
+            select(csn_cf) %>% filter(row_number()<=time_hz)
+        
+        while (nrow(csn_cf) < time_hz) {
+            # adds zeros to the csn payments after it is done
+            csn_cf <- rbind(csn_cf, 0)
+        }
+        
+        df <- cbind(df, csn_cf)
+        
+        # Saving
+        df <- df %>% mutate(savings = wage - consumption - csn_cf)
+        
+        # Assets
+        df <- df %>% mutate(assets = assets_seq(savings, 0.07, time_hz, input$start_wealth))
+        
+        # Financial independence
+        df <- df %>% mutate(findependence = 25*consumption)
+        
+        # Plot
+        plot.findep <- df %>% select(year, assets, findependence) %>% 
+            gather(key = "Series", value = "SEK", -year) %>%
+            ggplot(aes(x = year, y = SEK)) +
+            geom_line(aes(color = Series), size = 1) +
+            theme_minimal() + 
+            colorpal
+        
+        plot.savings <- df %>% select(year, savings, csn_cf) %>% 
+            gather(key = "Series", value = "SEK", -year) %>% 
+            ggplot(aes(x = year, y = SEK/12)) +
+            geom_line(aes(color = Series)) + theme_minimal() +
+            labs(y = "Monthly savings") +
+            colorpal
+            
+        plot.findep / plot.savings# TODO: add other information to the dashboard
+        # Total debt (csn + mortgage)
+        # Total debt to equity ratio
+        
     })
     
 }
---
+++ code/aux_functions.R
@@ -0,0 +1,24 @@
+# Wage ####
+growth.seq <- function(g = 0, time_hz = 25, start_value){
+  
+  years <- 1:time_hz
+  sequence <- rep(start_value*12, length(years))*exp(g*years)
+  
+  return(sequence)
+}
+
+
+# Savings ####
+assets_next <- function(assets, growth, savings){
+  a_next <- assets*(1+growth) + savings
+  return(a_next)
+}
+
+assets_seq <- function(savings, a_g, time_hz, a_start){
+  assets <- numeric(time_hz)
+  assets[1] <- a_start + savings[1]
+  for (year in 2:time_hz) {
+    assets[year] <- assets[year-1] * (1+a_g) + savings[year]
+  }
+  return(assets)
+}
